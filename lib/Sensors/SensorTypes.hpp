e00985698ad87adbbaf9bff9194f875562e2281d
SVNN <sylvainngacham59@gmail.com>
2026-03-04 23:00:54 -0500

    Add BLE UUID definitions and refactor sensor types; remove unused files

--- lib/Sensors/SensorTypes.hpp
+++ lib/Sensors/SensorTypes.hpp
@@ -7,13 +7,6 @@ All enumerated sensors for this allpication
 
 #pragma once
 
-enum class SensorType {
-    LIDAR,
-    LINE_LEFT,
-    LINE_RIGHT,
-    ULTRASONIC,
-    TOTAL  // toujours en dernier pour connaître le nombre de capteurs
-};
 
 // Structure envoyée via la queue
 
@@ -22,4 +15,20 @@ struct SensorData {
     float lineLeft;
     float lineRight;
     float ultrasonic;
+};
+
+enum class SensorType
+{
+    LIDAR,
+    LINE,
+    ULTRASONIC
+};
+
+enum class SensorPosition
+{
+    LEFT,
+    RIGHT,
+    FRONT,
+    BACK,
+    CENTER
 };
